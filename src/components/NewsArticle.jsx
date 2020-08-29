637c93ff13eb2d1aae1eabd3fbab50b2f321410c
Sreevathsa <sreevathsagowru@gmail.com>
2020-08-29 20:37:57 +0530

    News articles styling v2

--- src/components/NewsArticle.jsx
+++ src/components/NewsArticle.jsx
@@ -4,19 +4,24 @@ import { Card, CardBody, CardTitle, CardSubtitle } from "reactstrap";
 class NewsArticle extends Component {
   constructor(props) {
     super(props);
+
+    this.visitArticle = this.visitArticle.bind(this);
+  }
+
+  visitArticle(e) {
+    window.open(this.props.url, "_blank");
   }
 
   render() {
     return (
       <>
-        <Card className="h-100 ">
-          <CardBody>
+        <Card className="caseDetailsCard col-md-10 link margin-top-10">
+          <CardBody onClick={this.visitArticle}>
             <CardTitle>
               <span className="font-weight-bold">{this.props.title}</span>
             </CardTitle>
             <CardSubtitle>
               <span>{this.props.description}</span>
-              <span>{this.props.content}</span>
             </CardSubtitle>
           </CardBody>
         </Card>
